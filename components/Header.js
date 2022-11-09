e731a90f23be6be28f0e960490cce168006cd2c9
Ozmundooo <osman.ali.dev@gmail.com>
2022-11-09 13:24:21 -0500

    added todos

--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Logo from '../assets/LogoDB.png'
 import Image from 'next/image'
 export default function Header() {
+  /*
+  TODO:
+  - Make pages
+  - Link pages
+  - Make Header responsive
+  */
   return (
     <div className='px-24 py-12 flex justify-between bg-black text-white'>
         <Image src={Logo}/>
         <div className='flex flex-row gap-14 text-xl'>
-            <h3>Link 1</h3>
-            <h3>Link 2</h3>
-            <h3>Link 3</h3>
+          
+            <h3>About</h3>
+            <h3>Contact</h3>
+            <h3>CV</h3>
         </div>
     </div>
   )
