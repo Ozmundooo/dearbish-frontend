8af741d47d76f18568400a18fd491b92f954cd4b
Ozmundooo <osman.ali.dev@gmail.com>
2022-11-27 05:02:41 -0500

    centered text - home

--- pages/home.js
+++ pages/home.js
@@ -12,28 +12,28 @@ export default function home() {
         </header>
         <div className="lg:w-3/4 grid gap-8 my-2 justify-center align-middle lg:grid-cols-2 lg:place-content-center lg:place-items-center lg:gap-16">
           <Link href="/personal">
-            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] text-center">
               <h2 className="text-xl font-semibold  font-['Poppins']">
                 Personal
               </h2>
             </div>
           </Link>
           <Link href="/personal">
-            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] text-center">
               <h2 className="text-xl font-semibold font-['Poppins']">
                 Editorials
               </h2>
             </div>
           </Link>
           <Link href="/personal">
-            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] text-center">
               <h2 className="text-xl font-semibold  font-['Poppins']">
                 Videos
               </h2>
             </div>
           </Link>
           <Link href="/personal">
-            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] text-center">
               <h2 className="text-xl font-semibold font-['Poppins']">
                 Creative Direction
               </h2>
