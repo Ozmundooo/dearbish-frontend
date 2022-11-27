447f71e103e67bd362613ccd1d3c8a8b30c9f56f
Ozmundooo <osman.ali.dev@gmail.com>
2022-11-26 22:28:24 -0500

    added home page

--- pages/home.js
+++ pages/home.js
@@ -10,7 +10,36 @@ export default function home() {
         <header className="lg:hidden">
           <Header />
         </header>
-        <div className="lg:w-3/4 "></div>
+        <div className="lg:w-3/4 grid gap-8 my-2 justify-center align-middle lg:grid-cols-2 lg:place-content-center lg:place-items-center lg:gap-16">
+          <Link href="/personal">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+              <h2 className="text-xl font-semibold  font-['Poppins']">
+                Personal
+              </h2>
+            </div>
+          </Link>
+          <Link href="/personal">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+              <h2 className="text-xl font-semibold font-['Poppins']">
+                Editorials
+              </h2>
+            </div>
+          </Link>
+          <Link href="/personal">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+              <h2 className="text-xl font-semibold  font-['Poppins']">
+                Videos
+              </h2>
+            </div>
+          </Link>
+          <Link href="/personal">
+            <div className="bg-white grid place-items-center w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+              <h2 className="text-xl font-semibold font-['Poppins']">
+                Creative Direction
+              </h2>
+            </div>
+          </Link>
+        </div>
         <aside className="lg:w-1/4 lg:h-screen sticky top-0 hidden lg:flex">
           <Header />
         </aside>
--- pages/personalprojects.js
+++ pages/personalprojects.js
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import { sanityClient, urlFor } from "../sanity";
-
 import { useState } from "react";
 
 export const PersonalProjects = ({ personals }) => {
@@ -22,17 +21,19 @@ export const PersonalProjects = ({ personals }) => {
                   {personal.title}
                 </h2>
                 <div className="flex lg:gap-12 md:gap-10 gap-6 overflow-x-scroll scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-black scroll-my-8 snap snap-mandatory snap-x">
-                  {personal.images.map((image) => (
+                  {personal.images.map((image, index) => (
                     <img
                       className="lg:h-[400px] snap-center"
                       src={urlFor(image)}
-                      key={image}
+                      key={index}
                     />
                   ))}
                 </div>
                 <p className="font-light lg:my-4 mt-3">{personal.subtext}</p>
                 <div className="flex flex-col justify-evenly">
-                  <h2 className="lg:text-xl my-1 ">{personal.description}</h2>
+                  <h2 className="lg:text-xl my-1 lg:mr-4 mr-2">
+                    {personal.description}
+                  </h2>
                   {personals.length - 1 !== index ? (
                     <div className=" grid place-items-center animate-pulse lg:mt-10 mt-5">
                       <p>Scroll for more ↓</p>
