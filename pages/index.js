fc394cd3b2e24b9e82f0ddb2ead9ca1f7c19ea47
Ozmundooo <osman.ali.dev@gmail.com>
2022-12-20 21:14:46 -0500

    enter page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
-import { sanityClient } from "../sanity";
 import Link from "next/link";
+import Bish from "../assets/enterBG.jpg";
+import Image from "next/image";
 const Home = ({ personals }) => {
   return (
     <>
-      <div className=" bg-black h-screen grid place-items-center ">
+      <div className=" bg-black h-screen grid place-items-center relative overflow-hidden">
+        <Image
+          className="h-screen w-screen scale-125 hover:scale-100 duration-300"
+          src={Bish}
+        />
         <Link
           href="/home"
-          className="text-white border border-white px-8 py-4 font-['Poppins'] font-semibold hover:bg-white hover:text-black duration-150"
+          className="absolute text-white group hover:scale-150 border border-white px-8 py-4 font-['Poppins'] font-semibold hover:bg-white hover:text-black duration-150"
         >
           ENTER
         </Link>
