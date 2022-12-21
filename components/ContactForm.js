e2f8e06c529fd7e65b9d21b04376fd9e295b66dd
Ozmundooo <osman.ali.dev@gmail.com>
2022-12-21 00:49:10 -0500

    contact form change

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -5,7 +5,7 @@ export default function ContactForm() {
     <div className="lg:justify-center lg:align-middle lg:flex ">
       <form
         className="flex flex-col p-10 align-middle lg:w-1/2 text-white "
-        action="https://formsubmit.co/info@osmanali.ca"
+        action="https://formsubmit.co/info@dearbish.com"
         method="POST"
       >
         <h2 className="lg:text-5xl text-3xl text-white text text-center mb-10 font-bold">
